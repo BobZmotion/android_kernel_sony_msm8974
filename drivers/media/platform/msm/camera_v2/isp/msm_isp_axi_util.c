deb3e581c77d98117478d3be7dbaaa49005ec3f3
Peter Liu <pingchie@codeaurora.org>
2013-12-19 01:06:39 -0800

    msm: camera: fix stream size for the UB allocation
    Plane size is calculated in userspace already.
    No need to divided by 2 again in kernel.
    
    Change-Id: I120f4396d1ab0197dd666e7f60660143e9f58d5a
    Signed-off-by: Peter Liu <pingchie@codeaurora.org>
    
    Conflicts:
    	drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
  * Copyright (C) 2013 Sony Mobile Communications AB.
  *
  * This program is free software; you can redistribute it and/or modify
@@ -204,7 +204,7 @@ static uint32_t msm_isp_axi_get_plane_size(
 				plane_cfg[plane_idx].output_width;
 		else
 			size = plane_cfg[plane_idx].output_height *
-				plane_cfg[plane_idx].output_width / 2;
+				plane_cfg[plane_idx].output_width;
 		break;
 	case V4L2_PIX_FMT_NV14:
 	case V4L2_PIX_FMT_NV41:
@@ -213,7 +213,7 @@ static uint32_t msm_isp_axi_get_plane_size(
 				plane_cfg[plane_idx].output_width;
 		else
 			size = plane_cfg[plane_idx].output_height *
-				plane_cfg[plane_idx].output_width / 8;
+				plane_cfg[plane_idx].output_width;
 		break;
 	case V4L2_PIX_FMT_NV16:
 	case V4L2_PIX_FMT_NV61:
